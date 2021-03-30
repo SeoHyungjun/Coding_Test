369333c80447fa5e0abe879143ada17dde213f7b
SeoHyungjun <wqwwq96@gmail.com>
2021-03-30 21:46:47 +0900

    백준 solved.ac silver2 특정거리의도시찾기 다익스트라 메모리 및 시간 해결 코드 추가

--- baekjun/solved.ac/level/silver/silver2/특정거리의도시찾기/메모리줄인_다익스트라.py
+++ baekjun/solved.ac/level/silver/silver2/특정거리의도시찾기/메모리줄인_다익스트라.py
@@ -14,21 +14,29 @@ for _ in range(m):
     graph[start-1].append(end-1)
 
 queue = []
-heapq.heappush(queue, (x-1, 0))
+heapq.heappush(queue, (0, x-1))
+check = [False] * n
+check[x-1] = True
 distances = [float('inf')] * n
 distances[x-1] = 0
 
+
 while queue:
-    city, distance = heapq.heappop(queue)
+    distance, city = heapq.heappop(queue)
 
     for ci in graph[city]:
-        if distance + 1 < distances[ci]:
+        if not check[ci] and distance + 1 < distances[ci]:
             distances[ci] = distance + 1
-            heapq.heappush(queue, (ci, distances[ci]))
+            heapq.heappush(queue, (distances[ci], ci))
+            check[city] = True
 
-if k not in distances:
+answer = []
+for i in range(n):
+    if distances[i] == k:
+        answer.append(i+1)
+
+if not answer:
     print(-1)
 else:
-    for i in range(n):
-        if distances[i] == k:
-            print(i+1)
+    for i in answer:
+        print(i)
