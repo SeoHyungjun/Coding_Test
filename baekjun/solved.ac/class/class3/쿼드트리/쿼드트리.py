c514a67df5d6ca08d84269e00eb5d926cfa9cc87
SeoHyungjun <wqwwq96@gmail.com>
2021-04-02 21:46:32 +0900

    백준 solved.ac class3 쿼드트리 추가

---
+++ baekjun/solved.ac/class/class3/쿼드트리/쿼드트리.py
@@ -0,0 +1,29 @@
+import sys
+
+board = []
+
+def all_equal(x, y, size):
+    tmp =board[x][y]
+    for i in range(size):
+        for j in range(size):
+            if board[x + i][y + j] != tmp:
+                return False
+    return True
+
+def check(x, y, size):
+    if all_equal(x, y, size):
+        return board[x][y]
+    else:
+        answer = ''
+        for i in range(2):
+            for j in range(2):
+                answer += check(x + i*size//2, y + j*size//2, size//2)
+        return '(' + answer + ')'
+
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    board.append(list(sys.stdin.readline().rstrip('\n')))
+
+print(check(0, 0, n))
