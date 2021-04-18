f7760e16ec50e1a6506a03ac80d0792d22e14def
SeoHyungjun <wqwwq96@gmail.com>
2021-04-18 21:25:35 +0900

    백준 solved.ac class4 특정한최단경로 추가

---
+++ baekjun/solved.ac/class/class4/특정한최단경로/특정한최단경로.py
@@ -0,0 +1,40 @@
+import sys
+import heapq
+
+N, E = map(int, sys.stdin.readline().split())
+
+graph = [[float('inf')]*N for _ in range(N)]
+for _ in range(E):
+    s, e, c = map(int, sys.stdin.readline().split())
+    graph[s-1][e-1] = c
+    graph[e-1][s-1] = c
+
+mid1, mid2 = map(int, sys.stdin.readline().split())
+mid1, mid2 = mid1-1, mid2-1
+
+def dijsktra(index):
+    queue = []
+    heapq.heappush(queue, (0, index))
+    distance = [float('inf')] * N
+    distance[index] = 0
+
+    while queue:
+        cost, next = heapq.heappop(queue)
+
+        if cost > distance[next]:
+            continue
+
+        for i in range(N):
+            if cost + graph[next][i] < distance[i]:
+                distance[i] = cost + graph[next][i]
+                heapq.heappush(queue, (distance[i], i))
+
+    return distance
+
+st = dijsktra(0)
+v1 = dijsktra(mid1)
+v2 = dijsktra(mid2)
+
+answer = min(st[mid1] + v1[mid2] + v2[N-1], st[mid2] + v2[mid1] + v1[N-1])
+
+print(answer if answer < float('inf') else -1)
