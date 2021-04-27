e8962a0110132b003d9fcdf8010f43bd2fa96623
SeoHyungjun <wqwwq96@gmail.com>
2021-04-27 21:29:58 +0900

    백준 solved.ac class4 가장긴증가하는부분수열(LIS) 추가

---
+++ baekjun/solved.ac/class/class4/가장긴증가하는부분수열/가장긴증가하는부분수열.py
@@ -0,0 +1,17 @@
+import sys
+import bisect
+
+N = int(sys.stdin.readline())
+
+arr = list(map(int, sys.stdin.readline().split()))
+
+dp = [arr[0]]
+
+for i in range(1, N):
+    if arr[i] > dp[-1]:
+        dp.append(arr[i])
+
+    else:
+        dp[bisect.bisect_left(dp, arr[i])] = arr[i]
+
+print(dp)
