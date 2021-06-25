0a1cf0e4d3b863f0183f2cec49d4d4927f48f64f
SeoHyungjun <wqwwq96@gmail.com>
2021-06-25 21:38:56 +0900

    백준 solved.ac class5 도시분할계획 함수화

--- baekjun/solved.ac/class/class5/도시분할계획/도시분할계획.py
+++ baekjun/solved.ac/class/class5/도시분할계획/도시분할계획.py
@@ -1,35 +1,38 @@
 import sys
 import heapq
 
-N, M = map(int, sys.stdin.readline().split())
+def solve():
+    N, M = map(int, sys.stdin.readline().split())
 
-edge = []
-parent = [i for i in range(N+1)]
+    edge = []
+    parent = [i for i in range(N+1)]
 
-for _ in range(M):
-    a, b, c = map(int, sys.stdin.readline().split())
-    edge.append((c, a, b))
+    for _ in range(M):
+        a, b, c = map(int, sys.stdin.readline().split())
+        edge.append((c, a, b))
 
-edge.sort()
+    edge.sort()
 
-def find(i):
-    if parent[i] != i:
-        parent[i] = find(parent[i])
-    return parent[i]
+    def find(i):
+        if parent[i] != i:
+            parent[i] = find(parent[i])
+        return parent[i]
 
-def union(a, b):
-    a = find(a)
-    b = find(b)
+    def union(a, b):
+        a = find(a)
+        b = find(b)
 
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+        if a < b:
+            parent[b] = a
+        else:
+            parent[a] = b
 
-answer = []
-for c, a, b in edge:
-    if find(a) != find(b):
-        union(a, b)
-        answer.append(c)
+    answer = []
+    for c, a, b in edge:
+        if find(a) != find(b):
+            union(a, b)
+            answer.append(c)
 
-print(sum(answer[:-1]))
+    print(sum(answer[:-1]))
+
+solve()
