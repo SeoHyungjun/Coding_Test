9fa0d25b78c9766bbc328a0ccc0137f8a6174bad
SeoHyungjun <wqwwq96@gmail.com>
2021-06-25 21:36:11 +0900

    백준 solved.ac class5 도시분할계획 추가

---
+++ baekjun/solved.ac/class/class5/도시분할계획/도시분할계획.py
@@ -0,0 +1,35 @@
+import sys
+import heapq
+
+N, M = map(int, sys.stdin.readline().split())
+
+edge = []
+parent = [i for i in range(N+1)]
+
+for _ in range(M):
+    a, b, c = map(int, sys.stdin.readline().split())
+    edge.append((c, a, b))
+
+edge.sort()
+
+def find(i):
+    if parent[i] != i:
+        parent[i] = find(parent[i])
+    return parent[i]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+answer = []
+for c, a, b in edge:
+    if find(a) != find(b):
+        union(a, b)
+        answer.append(c)
+
+print(sum(answer[:-1]))
