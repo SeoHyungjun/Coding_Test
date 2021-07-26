bd9df900647bdc144d84e3c5a103219b7f3c780b
SeoHyungjun <wewwe96@naver.com>
2021-07-26 17:54:01 +0900

    update 백준 solved.ac class4 스티커

--- baekjun/solved.ac/class/class4/스티커/스티커.py
+++ baekjun/solved.ac/class/class4/스티커/스티커.py
@@ -9,11 +9,15 @@ for _ in range(T):
     for _ in range(2):
         board.append(list(map(int, sys.stdin.readline().split())))
 
-    board[0][1] += board[1][0]
-    board[1][1] += board[0][0]
+    if N == 1:
+        print(max(board[0][0], board[1][0]))
 
-    for i in range(2, N):
-        board[0][i] += max(board[1][i-1], board[1][i-2])
-        board[1][i] += max(board[0][i-1], board[0][i-2])
+    else:
+        board[0][1] += board[1][0]
+        board[1][1] += board[0][0]
 
-    print(max(board[0][N-1], board[1][N-1]))
+        for i in range(2, N):
+            board[0][i] += max(board[1][i-1], board[1][i-2])
+            board[1][i] += max(board[0][i-1], board[0][i-2])
+
+        print(max(board[0][N-1], board[1][N-1]))
