8d4d22da5a30dacd4ec44e7264f38d4dacc7d950
SeoHyungjun <wewwe96@naver.com>
2021-09-08 19:26:35 +0900

    Add : 나부 함대 데이터 of solved.ac bronze5

---
+++ baekjun/solved.ac/level/bronze/bronze5/나부 함대 데이터/나부함대데이터.py
@@ -0,0 +1,6 @@
+print('%-15s%-15s%-11s%-10s' % ('SHIP NAME', 'CLASS', 'DEPLOYMENT', 'IN SERVICE'))
+print('%-15s%-15s%-11s%-10s' % ('N2 Bomber', 'Heavy Fighter', 'Limited', '21'))
+print('%-15s%-15s%-11s%-10s' % ('J-Type 327', 'Light Combat', 'Unlimited', '1'))
+print('%-15s%-15s%-11s%-10s' % ('NX Cruiser', 'Medium Fighter', 'Limited', '18'))
+print('%-15s%-15s%-11s%-10s' % ('N1 Starfighter', 'Medium Fighter', 'Unlimited', '25'))
+print('%-15s%-15s%-11s%-10s' % ('Royal Cruiser', 'Light Combat', 'Limited', '4'))
