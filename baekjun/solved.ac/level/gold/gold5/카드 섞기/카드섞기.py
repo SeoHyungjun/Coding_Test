3b18a8de3edad68bf4b6b526a82a289630ea5562
SeoHyungjun <wewwe96@naver.com>
2021-09-26 18:30:30 +0900

    Add : 카드 섞기 of solved.ac gold5

---
+++ baekjun/solved.ac/level/gold/gold5/카드 섞기/카드섞기.py
@@ -0,0 +1,29 @@
+import sys
+
+def changeArr(arr):
+    newArr = arr[:]
+    for i in range(N):
+        newArr[i] = arr[S[i]]
+
+    return newArr
+
+def shuffle():
+    cnt = 0
+    arr = [i%3 for i in range(N)]
+    firstArr = arr[:]
+
+    while arr != P:
+        arr = changeArr(arr)
+        cnt += 1
+
+        if firstArr == arr:
+            return -1
+
+    return cnt
+
+
+N = int(sys.stdin.readline())
+P = list(map(int, sys.stdin.readline().split()))
+S = list(map(int, sys.stdin.readline().split()))
+
+print(shuffle())
