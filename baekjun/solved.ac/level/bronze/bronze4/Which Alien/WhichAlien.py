605cae2baad22ff7dda6b8eb08feb728eb9f29f3
SeoHyungjun <wewwe96@naver.com>
2021-10-28 21:34:08 +0900

    Add : Which Alien of solved.ac bronze4

---
+++ baekjun/solved.ac/level/bronze/bronze4/Which Alien/WhichAlien.py
@@ -0,0 +1,11 @@
+import sys
+
+antenna = int(sys.stdin.readline())
+eyes = int(sys.stdin.readline())
+
+if antenna >= 3 and eyes <= 4:
+    print('TroyMartian')
+if antenna <= 6 and eyes >= 2:
+    print('VladSaturnian')
+if antenna <= 2 and eyes <= 3:
+    print('GraemeMercurian')
