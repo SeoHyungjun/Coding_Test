16b0953dabfeb60646a829a82edcad0291051aa2
SeoHyungjun <wewwe96@naver.com>
2022-06-09 18:56:39 +0900

    ADD : 게임 of solved.ac silver4

---
+++ baekjun/solved.ac/level/silver/silver4/게임/게임.py
@@ -0,0 +1,19 @@
+import sys
+
+X, Y = map(int, sys.stdin.readline().split())
+Z = (Y * 100) // X
+if Z >= 99:
+    print(-1)
+else:
+    answer = 0
+    left = 1
+    right = X
+    while left <= right:
+        mid = (left + right) // 2
+        if (Y+mid)*100 // (X+mid) <= Z:
+            left = mid+1
+        else:
+            answer = mid
+            right = mid - 1
+
+    print(answer)
