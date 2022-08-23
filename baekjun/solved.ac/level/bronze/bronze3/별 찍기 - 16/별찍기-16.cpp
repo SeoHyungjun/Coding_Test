2a8e05edfbab68c964336d4dcdf602d31dec1440
SeoHyungjun <wewwe96@naver.com>
2022-08-23 21:15:26 +0900

    ADD : 별 찍기 - 16 of solved.ac bronze3

---
+++ baekjun/solved.ac/level/bronze/bronze3/별 찍기 - 16/별찍기-16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j < N - i; j++) cout << " ";
+        for (int j = 0; j < i; j++) cout << "* ";
+        cout << "\n";
+    }
+
+    return 0;
+}
