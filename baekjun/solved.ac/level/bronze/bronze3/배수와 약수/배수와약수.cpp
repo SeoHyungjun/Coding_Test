c7209716cb251f2e6c24fe4797d664b23d92aa35
SeoHyungjun <wewwe96@naver.com>
2022-08-23 21:14:56 +0900

    ADD : 배수와 약수 of solved.ac bronze3

---
+++ baekjun/solved.ac/level/bronze/bronze3/배수와 약수/배수와약수.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    while (1) {
+        cin >> a >> b;
+        if (a == 0 and b == 0) break;
+
+        if (b % a == 0) cout << "factor" << endl;
+        else if (a % b == 0) cout << "multiple" << endl;
+        else cout << "neither" << endl;
+    }
+
+    return 0;
+}
