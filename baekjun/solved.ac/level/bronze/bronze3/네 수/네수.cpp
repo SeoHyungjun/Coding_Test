a80d0b3e78b63a4cba83f7e23b19eed4ac8b3217
SeoHyungjun <wewwe96@naver.com>
2022-08-23 21:31:44 +0900

    ADD : 네 수 of solved.ac bronze3

---
+++ baekjun/solved.ac/level/bronze/bronze3/네 수/네수.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b, c, d;
+    
+    cin >> a >> b >> c >> d;
+    cout << stoll(a + b) + stoll(c + d);
+
+    return 0;
+}
