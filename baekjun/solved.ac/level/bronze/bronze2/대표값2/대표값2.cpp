6526d0f8dcfd442fc68585a66e3bac02ea1083fc
SeoHyungjun <wewwe96@naver.com>
2022-08-26 20:25:08 +0900

    ADD : 대표값2 of solved.ac bronze2

---
+++ baekjun/solved.ac/level/bronze/bronze2/대표값2/대표값2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr[5], sum = 0;
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    sort(arr, arr+5);
+
+    cout << sum / 5 << endl << arr[2];
+
+    return 0;
+}
