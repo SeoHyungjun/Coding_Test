80ce8916faa4add6a8c8e8427f076b52fda5c5ce
SeoHyungjun <wewwe96@naver.com>
2022-09-09 16:32:42 +0900

    ADD : 유학 금지 of solved.ac bronze2

---
+++ baekjun/solved.ac/level/bronze/bronze2/유학 금지/유학금지.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+    std::string str, answer = "";
+
+    std::cin >> str;
+    for (int i = 0; i < str.length(); ++i) {
+        if (str[i] == 'C' || str[i] == 'A' || str[i] == 'M' || str[i] == 'B' || str[i] == 'R' || str[i] == 'I' || str[i] == 'D' || str[i] == 'G' || str[i] == 'E') continue;
+        answer += str[i];
+    }
+
+    std::cout << answer;
+
+    return 0;
+}
