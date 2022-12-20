fbbb01e3f0bb45c15855c9aaaac8dc5657824269
SeoHyungjun <wewwe96@naver.com>
2022-12-20 20:27:02 +0900

    ADD : 직각 삼각형의 두 변 of solved.ac bronze3

---
+++ baekjun/solved.ac/level/bronze/bronze3/직각 삼각형의 두 변/직각삼각형의두변.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double a, b, c;
+    int i = 1;
+    while (1)
+    {
+        double ans = 0;
+        cin >> a >> b >> c;
+        if (!a && !b && !c)
+            break;
+        cout << "Triangle #" << i << '\n';
+        i++;
+        if (a == -1)
+        {
+            ans = sqrt(c * c - b * b);
+            if (ans > 0)
+                printf("a = %.3f\n", ans);
+            else
+                cout << "Impossible.\n";
+        }
+        else if (b == -1)
+        {
+            ans = sqrt(c * c - a * a);
+            if (ans > 0)
+                printf("b = %.3f\n", ans);
+            else
+                cout << "Impossible.\n";
+        }
+        else
+ 
+        {
+            ans = sqrt(a * a + b * b);
+            if (ans > 0)
+                printf("c = %.3f\n", ans);
+            else
+                cout << "Impossible.\n";
+        }
+        cout << '\n';
+    }
+}
